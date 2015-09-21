904b8867be1df9d64eeb5df99cfdc18252f58c43
Matt Hancock <mhancock743@gmail.com>
2015-09-21 14:05:29 -0400

    big changes to func-iter, improved coloring efficiency

--- func-iter/main.js
+++ func-iter/main.js
@@ -1,115 +1,161 @@
-generate_rand_color_scheme = function(n) {
-	color_scheme = [];
-	for ( i=1; i <= n; i += 1 ) {
-		color_scheme[i] = 'rgb('+String( Math.floor( Math.random()*255 ))+','+String( Math.floor( Math.random()*255 ))+','+String( Math.floor( Math.random()*255 ))+')';
-	}
-	return color_scheme;
-}
-
-color = function( colors, points ) {
-	for( i=0; i < points.length; i+=1 ) {
-		for( j=0; j < points[0].length; j+=1 ) {
-			c.fillStyle = colors[ points[i][ points[0].length-1-j ] ];
-			c.fillRect(i,j,1,1);
-		}
-	}
-}
-
-var g_func = "x*x - y*y + 0.285";
-var h_func = "2*x*y - 0.55";
-
-var stop = false; 
-update_frac = function() {
-	frac.con_limit = parseInt( $('#convergence_limit').val() );
-	frac.div_limit = parseInt( $('#divergence_limit').val() );
-	
-	if ( isNaN($('#canvas_width').val()) ) { stop = true; return false; }
-	else { $('#js-frac')[0].width = parseInt( $('#canvas_width').val() ); }
-	if ( isNaN($('#canvas_height').val()) ) { stop = true; return false; }
-	else { $('#js-frac')[0].height = parseInt( $('#canvas_height').val() ); }
-
-	frac.width = parseInt( $('#js-frac').width() );
-	frac.height = parseInt( $('#js-frac').height() );
-
-	frac.xMin = parseFloat( $('#frac-table input.xMin:first').val() );
-	frac.xMax = parseFloat( $('#frac-table input.xMax:first').val() );
-	frac.yMin = parseFloat( $('#frac-table input.yMin:first').val() );
-	frac.yMax = parseFloat( $('#frac-table input.yMax:first').val() );
-	
-	frac.xStep = ( frac.xMax - frac.xMin ) / (frac.width - 1);
-	frac.yStep = ( frac.yMax - frac.yMin ) / (frac.height - 1);
-	
-	f = "( function(x) { return [";
-	f += $('#g_func').val().replace(/x/g,'x[0]').replace(/y/g,'x[1]').replace(/ex\[0\]p/g,'exp') + ",";
-	f += $('#h_func').val().replace(/x/g,'x[0]').replace(/y/g,'x[1]').replace(/ex\[0\]p/g,'exp');
-	f += "]; } )";
-	stop = false;
-	try {
-		eval( f )([0,0]);
-	} catch ( e ) {
-		if ( e instanceof SyntaxError ) alert( "There was a syntax error in one of the functions." );
-		if ( e instanceof ReferenceError ) alert( "There was a reference error in one of the functions. Did you forget \"Math.\" before a math function?" );
-		stop = true;
-	}
-	if ( !stop ) frac.f = f; 
-}
-
-var points = "points";
-
-c = document.getElementById('js-frac').getContext('2d');
-
-frac = {
-	f: "( function(x) { return [x[0]*x[0]-x[1]*x[1] + 0.285, 2* x[0]*x[1] - 0.55  ] }  )",
-	con_limit: 100,
-	div_limit: 100,
-	
-	width: document.getElementById('js-frac').width,
-	height: document.getElementById('js-frac').height,
-	
-	xMin: -1.0,
-	xMax: 1.0,
-	xStep: 2.0 / (document.getElementById('js-frac').width-1),
-
-	yMin: -1.0,
-	yMax: 1.0,
-	yStep: 2.0 / (document.getElementById('js-frac').height-1)
-};
-
-var worker = "";
-function makeWorker() {
-	worker = new Worker('./make-frac.js');
-	worker.addEventListener('message', function(e) {
-		if ( isNaN( e.data ) ) {
-			points = e.data;
-			color( generate_rand_color_scheme( frac.con_limit ), e.data );
-			$('#frac-loading').html("Done.");
-		}
-		else { 
-			$('#frac-loading').html( Math.round( ((e.data+1) / frac.width)*100 ) +"%" );
-		}
-    }, false);
-};
-
-makeWorker();
-worker.postMessage( JSON.stringify(frac) );
-
 $(function() {
-	$('input.xMin').val( frac.xMin );
-	$('input.xMax').val( frac.xMax );
-	$('input.yMin').val( frac.yMin );
-	$('input.yMax').val( frac.yMax );
+    var g_func = "x*x - y*y + 0.285";
+    var h_func = "2*x*y - 0.55";
 
+    canvas  = $('#js-frac')[0]
+    context = canvas.getContext('2d');
+
+    // fractal container object
+    frac = {
+        f: "( function(x) { return [x[0]*x[0]-x[1]*x[1] + 0.285, 2* x[0]*x[1] - 0.55  ] }  )",
+        con_limit: 100,
+        div_limit: 100,
+        
+        width:  canvas.width,
+        height: canvas.height,
+        
+        xMin: -1.0,
+        xMax: 1.0,
+        xStep: 2.0 / (canvas.width-1),
+
+        yMin: -1.0,
+        yMax: 1.0,
+        yStep: 2.0 / (canvas.height-1),
+
+        // This array stores the number of iterations a point
+        // 'survives' the functional iteration.
+        points: new Uint16Array(canvas.width*canvas.height),
+
+        // worker communication flag
+        color_only: false
+    };
+
+    var worker = "this string means nothing"; // initialize
+    function make_worker() {
+        worker = new Worker('./make-frac.js');
+        $('#frac-loading').html('');
+
+        worker.addEventListener('message', function(msg) {
+            switch ( msg.data.topic ) {
+                case 'testing':
+                    // The message is a status update on the percent completion of point testing.
+                    var pc = parseFloat(msg.data.iter);
+                    pc = Math.round( ((pc+1) / frac.width)*100 ) 
+                    $('#frac-loading').html( 'Testing points: ' + pc + '%' );
+                    break;
+                case 'frac':
+                    // Recieving updated frac object.
+                    frac = JSON.parse( msg.data.frac )
+                    $('#frac-loading').html('');
+                    if ( $('#draw').css('display') == 'none' )
+                        $('#draw, #stop').toggle();
+                    break;
+                case 'image_data':
+                    id = new ImageData(frac.width, frac.height);
+                    id.data.set(msg.data.image_data);
+                    context.putImageData(id, 0, 0)
+                    break;
+                default:
+                    throw 'Incoming message had no topic.';
+            }
+
+        }, false);
+    };
+    make_worker();
+    worker.postMessage( JSON.stringify(frac) );
+
+    // DOM init stuff / event responders.
+    $('input.xMin').val( frac.xMin );
+    $('input.xMax').val( frac.xMax );
+    $('input.yMin').val( frac.yMin );
+    $('input.yMax').val( frac.yMax );
     $('#g_func').val( g_func );
     $('#h_func').val( h_func );
+    $('#convergence_limit').val( frac.con_limit );
+    $('#divergence_limit').val( frac.div_limit );
+    $('#canvas_width').val( frac.width );
+    $('#canvas_height').val( frac.height );
 	
-	$('#convergence_limit').val( frac.con_limit );
-	$('#divergence_limit').val( frac.div_limit );
-	
-	$('#canvas_width').val( frac.width );
-	$('#canvas_height').val( frac.height );
-	
-	$('body').on('change', '#frac-table input', function() {
-		if ( isNaN( $(this).val() ) ) return;
-		$('.'+$(this).attr('class')).val( $(this).val() );
-	});
+    // Error checking for integer inputs.
+    $('body').on('change', '.integer-input', function() {
+        if ( isNaN( $(this).val() ) ) {
+            var tmpval = this.id.startsWith('canvas') ? 600 : 100;
+            $(this).val(tmpval);
+            alert('Invalid input. Value set to '+tmpval+' for the time being.');
+        }
+        else { // all good, but set to integer in case float was entered.
+            $(this).val( parseInt($(this).val()) );
+        }
+    });
+
+    $('body').on('change', 'input.xMin, input.xMax, input.yMin, input.yMax', function() {
+        if ( isNaN( $(this).val() ) ) {
+            alert('Non-number enter for mathematical bound.');
+            $('input.'+$(this).attr('class')).val( ($(this).attr('class').endsWith('Max')) ? 1 : -1 )
+        }
+        $('input.'+$(this).attr('class')).val( $(this).val() );
+    });
+
+    $('body').on('click', '#stop', function() {
+        worker.terminate();
+        make_worker();
+        $('#draw, #stop').toggle();
+    });
+
+    $('body').on('click', '#draw', function() {
+        update_frac(); 
+        $('#draw, #stop').toggle()
+        if (!stop) worker.postMessage(JSON.stringify(frac));
+    });
+
+    $('body').on('click', '#recolor', function() {
+        frac.color_only = true;
+        worker.postMessage(JSON.stringify(frac));
+    });
+
+    var stop = false; 
+    update_frac = function() {
+        // Set the convergence and divergence limits from values.
+        frac.con_limit = parseInt( $('#convergence_limit').val() );
+        frac.div_limit = parseInt( $('#divergence_limit').val() );
+
+        // Set canvas width and height as well as frac object data.
+        $('#js-frac')[0].width = parseInt( $('#canvas_width').val() );
+        $('#js-frac')[0].height = parseInt( $('#canvas_height').val() );
+        frac.width = parseInt( $('#js-frac').width() );
+        frac.height = parseInt( $('#js-frac').height() );
+
+        // Set the limits of the mathematical region of the canvas.
+        frac.xMin = parseFloat( $('#frac-table input.xMin:first').val() );
+        frac.xMax = parseFloat( $('#frac-table input.xMax:first').val() );
+        frac.yMin = parseFloat( $('#frac-table input.yMin:first').val() );
+        frac.yMax = parseFloat( $('#frac-table input.yMax:first').val() );
+        
+        // Set the resolution of the canvas.
+        frac.xStep = ( frac.xMax - frac.xMin ) / (frac.width - 1);
+        frac.yStep = ( frac.yMax - frac.yMin ) / (frac.height - 1);
+        
+        // Now we build the vector valued function, f, to be iterated
+        // from the two inputs from the dom.
+        f = "( function(x) { return [";
+        // the last replacement here is a workaround for the 'x' getting replaced in 'exp'.
+        f += $('#g_func').val().replace(/x/g,'x[0]').replace(/y/g,'x[1]').replace(/ex\[0\]p/g,'exp') + ",";
+        f += $('#h_func').val().replace(/x/g,'x[0]').replace(/y/g,'x[1]').replace(/ex\[0\]p/g,'exp');
+        f += "]; } )";
+
+        // Next, we do a try/catch of an eval of the function to test it for errors,
+        // and set the stop flag to true to prevent the worker from proceeding.
+        stop = false;
+        try {
+            eval( f )([0,0]);
+        } catch ( e ) {
+            if ( e instanceof SyntaxError )
+                alert( "There was a syntax error in one of the functions." );
+            if ( e instanceof ReferenceError )
+                alert( "Reference error. Did you forget \"Math.\" before a math function?" );
+            stop = true;
+        }
+        if ( !stop ) frac.f = f; 
+    }
+
 });
--- func-iter/make-frac.js
+++ func-iter/make-frac.js
@@ -1,26 +1,52 @@
-self.addEventListener('message', function( msg ) {
+generate_rand_color_scheme = function(n) {
+	color_scheme = new Array(n);
+	for (var i=0; i < n; i += 1) {
+		color_scheme[i] = new Array(3);
+        for (var j=0; j < 3; j+=1)
+            color_scheme[i][j] = Math.floor( Math.random()*255 )
+	}
+
+	return color_scheme;
+}
+
+self.onmessage = function(msg) {
 	var frac = JSON.parse( msg.data );
 	frac.f = eval( frac.f );
-	
-	var points = [];
-	var k=0;
-	for( var i=0; i < frac.width; i+=1 ) {
-		points[i] = [];
-		for( j=0; j < frac.height; j+=1 ) {
-			
-			k = 0;
-			var p = [ frac.xMin + i*frac.xStep, frac.yMin + j*frac.yStep ];
-			
-			while ( Math.sqrt(p[0]*p[0] + p[1]*p[1]) < frac.div_limit ) {
-				p = frac.f( p );
-				k += 1;
-				if ( k >= frac.con_limit ) break;
-			}
-		
-			points[i][j] = k;
-		}
-		self.postMessage( i );
-	}
-	
-	self.postMessage( points );
-}, false );
+
+    var colors = generate_rand_color_scheme( frac.con_limit );
+    var img_data = new Uint8ClampedArray(4*frac.width*frac.height);
+
+    for( var i=0; i < frac.width; i+=1 ) {
+        for( var j=0; j < frac.height; j+=1 ) {
+            var ip = i*frac.height + j
+            if ( !frac.color_only ) {
+                // This is the point to be iterated.
+                var p = [ frac.xMin + j*frac.xStep, frac.yMax - i*frac.yStep ];
+                
+                // While the norm of the current iterate is less than
+                // the divergence limit, keep iterating.
+                var k = 0;
+                while ( Math.sqrt(p[0]*p[0] + p[1]*p[1]) < frac.div_limit ) {
+                    p = frac.f( p ); k += 1;
+                    if ( k >= frac.con_limit ) break;
+                }
+            
+                frac.points[ip] = k;
+            }
+
+            for (var l=0; l<3; l+=1)
+                 // we have to offset by 1 because every point survices at least one iteration
+                img_data[4*ip+l] = colors[frac.points[ip]-1][l];
+            img_data[4*ip+3] = 255
+        }
+        if ( !frac.color_only ) self.postMessage({topic: 'testing', iter: i});
+    }
+    
+    // Send back the frac object
+    if ( frac.color_only ) frac.color_only = false;
+    self.postMessage({topic: 'frac', frac: JSON.stringify(frac)});
+
+    // Finally we send back the image data
+    self.postMessage({topic: 'image_data', image_data: img_data})
+
+}//, false );
